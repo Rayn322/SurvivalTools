c3919c6c26e6d3f8cdc9ef517c3bd4966efd104d
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2022-08-02 15:38:11 -0700

    Switch to Paper experimental chat decoration api

--- src/main/java/me/ryanlauderbach/survivaltools/listener/ChatFormatter.kt
+++ src/main/java/me/ryanlauderbach/survivaltools/listener/ChatFormatter.kt
@@ -1,33 +1,18 @@
 package me.ryanlauderbach.survivaltools.listener
 
-import io.papermc.paper.event.player.AsyncChatEvent
-import net.kyori.adventure.text.Component
+import io.papermc.paper.event.player.AsyncChatDecorateEvent
 import net.kyori.adventure.text.minimessage.MiniMessage
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.player.AsyncPlayerChatPreviewEvent
 
 class ChatFormatter : Listener {
 	
 	@EventHandler
-	fun onChatMessage(event: AsyncChatEvent) {
+	fun onChatMessage(event: AsyncChatDecorateEvent) {
 		val miniMessage = MiniMessage.miniMessage()
-		val plainMessage = PlainTextComponentSerializer.plainText().serialize(event.message())
-		val parsed: Component = miniMessage.deserialize(plainMessage)
-		
-		event.message(parsed)
-	}
-	
-	@EventHandler
-	fun onChatMessage(event: AsyncPlayerChatPreviewEvent) {
-		val miniMessage = MiniMessage.miniMessage()
-		val plainMessage = event.message
-		val parsed: Component = miniMessage.deserialize(plainMessage)
-		val serialized = LegacyComponentSerializer.legacySection().serialize(parsed)
-		
-		event.message = serialized
+		val plainMessage = PlainTextComponentSerializer.plainText().serialize(event.originalMessage())
+		event.result(miniMessage.deserialize(plainMessage))
 	}
 }
 
